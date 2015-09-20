03bd1240da27c726f062ab7a554331ee8f39c8e0
nrhubbar <nhubbard@localhost.localdomain>
2015-09-20 16:35:35 -0400

    Put the hide text in weather

--- html/weather.js
+++ html/weather.js
@@ -1,5 +1,6 @@
 
 function getWeather(){
+	document.getElementsById('ttbh').hide();
 	var webdriver = require('selinium-webdriver')
 	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chromedriver()).build()
 

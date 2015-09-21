fd29fb178a3b314696f7a616331f46bd573822f6
nrhubbar <14hubbardn@gmail.com>
2015-09-20 21:31:45 -0400

    added get weather

--- html/weather.js
+++ html/weather.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
-	// log.console("In getWeather");
-	// var webdriver = require('selinium-webdriver')
-	// var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chromedriver()).build()
 
-	// driver.get('http://www.google.com')
 	$("#weatherBtn").click(function(){
 		console.log("Hey");
 		//$.post("weather.js");
 		$("small").hide();
 	});
 	$('#bringBack').click(function(){
-		$("small").show();
+		getWeather();
+		console.log("finished getWeather")
 	});
 
-});	
+});
+
+function getWeather(){
+	console.log("In getWeather");
+	var webdriver = require('selinium-webdriver');
+	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chromedriver()).build();
+
+	driver.get('http://www.google.com');
+}

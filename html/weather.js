05c51b6af6678a87d74a2fdd02c5bffb9963d44e
nrhubbar <nhubbard@localhost.localdomain>
2015-09-20 20:27:10 -0400

    Using more jquery

--- html/weather.js
+++ html/weather.js
@@ -1,8 +1,13 @@
+$(document).ready(function(){
+	// log.console("In getWeather");
+	// var webdriver = require('selinium-webdriver')
+	// var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chromedriver()).build()
 
-function getWeather(){
-	log.console("In getWeather");
-	var webdriver = require('selinium-webdriver')
-	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chromedriver()).build()
+	// driver.get('http://www.google.com')
+	$("#weatherBtn").click(function(){
+		log.console("Hey");
+		//$.post("weather.js");
+		$("small").hide();
+	});
 
-	driver.get('http://www.google.com')
-}	
+});	

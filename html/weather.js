a3bd5a1c038e12000711bf3f6c656437f0763b96
nrhubbar <14hubbardn@gmail.com>
2015-09-22 23:08:30 -0400

    added a function to the get

--- html/weather.js
+++ html/weather.js
@@ -6,7 +6,9 @@ $(document).ready(function(){
 		$("small").hide();
 	});
 	$('#bringBack').click(function(){
-		$.get(127.0.0.0:3000);
+		$.get("127.0.0.0:3000","",function(){
+			$.("small").show();
+		});
 		console.log("finished getWeather")
 	});
 
